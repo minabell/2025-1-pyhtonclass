9ceb7ae8336ffde0ef8ed48125960cf9d723dfd4
310-0 <rldnr229@gmail.com>
2025-04-01 11:42:20 +0900

    파이구하기와 그래프그리기

--- p01py.py
+++ p01py.py
@@ -16,15 +16,19 @@ n = 2
 p = p*((2 * n + 1)**2 -1) / ((2 * n + 1)**2)
 print(p*4)
 
+pilist = [
+
+]
 #루프로 변환
 p=1
-for n in range(1,10):
+n=1
+for n in range(1,10000):
     p = p * ((2 * n + 1) ** 2 - 1) / ((2 * n + 1) ** 2)
 
-print(p*4)
+   # print(p*4)
+
+    pilist.append(p*4)
 
 import matplotlib.pyplot as plt
-plt.plot([1,3,4])
+plt.plot(pilist)
 plt.show()
-
-import opency
