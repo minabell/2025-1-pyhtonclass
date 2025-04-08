7a49082228652c5590ba4af901e969ccccdf280d
310-0 <rldnr229@gmail.com>
2025-04-08 11:45:41 +0900

    3번째 큰값을 갖는 파이썬 프로그램

--- p02CCollect.py
+++ p02CCollect.py
@@ -19,7 +19,7 @@ for n in range(1, 100):
         maxvaluen = n
 
 print(f'{maxvalue=}, {maxvaluen=}')
-#ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+#두번째 큰 값을 찾는 파이썬 프로그램
 maxvalue = 0
 maxvaluen = 0
 secondvalue = 0
@@ -47,5 +47,41 @@ for start_n in range(1, 100):
         secondn = start_n
 
 print(f'2등: {secondn} → {secondvalue}번')
+thirdvalue = 0
+thirdn = 0
+
+first = 0
+second = 0
+
+for start_n in range(1, 100):
+    n = start_n
+    ncount = 0
+
+    while n != 1:
+        if n % 2 == 1:
+            n = 3 * n + 1
+        else:
+            n = n // 2
+        ncount += 1
+
+    if ncount > first:
+        thirdvalue = second
+        thirdn = secondn if 'secondn' in locals() else 0
+        second = first
+        secondn = start_n
+        first = ncount
+
+    elif ncount > second:
+        thirdvalue = second
+        thirdn = secondn
+        second = ncount
+        secondn = start_n
+
+    elif ncount > thirdvalue:
+        thirdvalue = ncount
+        thirdn = start_n
+
+print(f'3등: {thirdn} → {thirdvalue}번')
+
 
 
